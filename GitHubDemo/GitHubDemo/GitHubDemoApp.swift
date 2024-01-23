0dcffe86a6c3cd2ee3b739012e8705cf00d175ba
Madhur Jain <madhur.jain@omniwyse.com>
2024-01-23 21:39:03 +0530

    Adding initial project

---
+++ GitHubDemo/GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by Madhur on 23/01/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
