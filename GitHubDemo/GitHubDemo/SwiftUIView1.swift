60adf51528686cf860de1ec514d043f8fa5d50be
Madhur Jain <madhur.jain@omniwyse.com>
2024-01-23 21:43:50 +0530

    Added SwiftUIView1

---
+++ GitHubDemo/GitHubDemo/SwiftUIView1.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView1.swift
+//  GitHubDemo
+//
+//  Created by Madhur on 23/01/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView1()
+}
